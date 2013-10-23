dc2071eea255d21f9ab7e0cf9087a516947444fb
Hunyady Márton <hunyadym@gmail.com>
2013-10-24 01:59:14 +0200

    Signin works

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,6 +10,10 @@ module SessionsHelper
     @current_user = user
   end
 
+  def signed_in?
+    !current_user.nil?
+  end
+
   def current_user
     remember_token = User.encrypt(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
